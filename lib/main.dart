aaef6512bd44cf1fce7e8303f4265a493861ce35
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2023-01-29 17:33:31 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/services.dart';
+import 'package:bring_the_menu/views/admin/signup/admin_signup.dart';
+import 'package:bring_the_menu/views/client/onBoard/onboard.dart';
+import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,12 +21,16 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      initialRoute: 'adminSignup',
+      theme: ThemeData(fontFamily: 'Lexend'),
+      getPages: [
+        GetPage(name: '/clientOnBoard', page: () => ClientOnBoard()),
+        GetPage(name: '/adminSignup', page: () => const AdminSignUp())
+      ],
     );
   }
 }

1f19051e247d04000d0f7491d1c300896f039ed8
sushant102004 <sushant.dhiman9812@gmail.com>
2023-01-29 09:21:53 +0530

    Added Fonts & Constants File

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Constants extends GetxController {
+  final backgroundColor = const Color.fromARGB(255, 19, 13, 17);
+  final whiteTextColor = const Color.fromARGB(255, 249, 249, 249);
+  final themeColor = const Color.fromARGB(255, 231, 123, 7);
+}

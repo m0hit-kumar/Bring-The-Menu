38c8daa52889087983818d77815facc8c5153ec6
sushant102004 <sushant.dhiman9812@gmail.com>
2023-01-30 11:25:11 +0530

    Update: Admin SignUp responsiveness.

--- lib/views/admin/signup/admin_signup.dart
+++ lib/views/admin/signup/admin_signup.dart
@@ -20,55 +20,57 @@ class _AdminSignUpState extends State<AdminSignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: constants.backgroundColor,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 7),
-        child: Container(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              SizedBox(height: Get.height / 6),
-              SvgPicture.asset(
-                'assets/images/foodPlate.svg',
-                width: Get.width / 1.4,
-              ),
-              SizedBox(height: Get.height / 13),
-              Text(
-                'Bring The Menu Admin',
-                style: TextStyle(
-                    color: constants.whiteTextColor,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 30),
-              ),
-              SizedBox(height: Get.height / 30),
-              Text(
-                'Go Digital',
-                style: TextStyle(color: constants.whiteTextColor, fontSize: 24),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: Get.height / 20),
-              InputWidget(
-                  constants: constants,
-                  title: 'Email',
-                  hintText: 'eg: youremail@gmail.com',
-                  controller: emailController,
-                  isObscrue: false),
-              SizedBox(height: Get.height / 20),
-              InputWidget(
-                  constants: constants,
-                  title: 'Password',
-                  hintText: 'eg: aStrongPassword#@',
-                  controller: passwordController,
-                  isObscrue: false),
-              SizedBox(height: Get.height / 20),
-              CustomButton(
-                  constants: constants,
-                  title: 'Sign Up',
-                  onTap: () {
-                    // To be implemented by Mohit Bhaiya.
-                  },
-                  width: Get.width / 3.2,
-                  height: Get.height / 20)
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 7),
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                SizedBox(height: Get.height / 10),
+                SvgPicture.asset(
+                  'assets/images/foodPlate.svg',
+                  width: Get.width / 1.4,
+                ),
+                SizedBox(height: Get.height / 13),
+                Text(
+                  'Bring The Menu Admin',
+                  style: TextStyle(
+                      color: constants.whiteTextColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 30),
+                ),
+                SizedBox(height: Get.height / 30),
+                Text(
+                  'Go Digital',
+                  style: TextStyle(color: constants.whiteTextColor, fontSize: 24),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: Get.height / 20),
+                InputWidget(
+                    constants: constants,
+                    title: 'Email',
+                    hintText: 'eg: youremail@gmail.com',
+                    controller: emailController,
+                    isObscrue: false),
+                SizedBox(height: Get.height / 20),
+                InputWidget(
+                    constants: constants,
+                    title: 'Password',
+                    hintText: 'eg: aStrongPassword#@',
+                    controller: passwordController,
+                    isObscrue: false),
+                SizedBox(height: Get.height / 20),
+                CustomButton(
+                    constants: constants,
+                    title: 'Sign Up',
+                    onTap: () {
+                      // To be implemented by Mohit Bhaiya.
+                    },
+                    width: Get.width / 3.2,
+                    height: Get.height / 20)
+              ],
+            ),
           ),
         ),
       ),

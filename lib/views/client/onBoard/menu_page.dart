81d4575cfc6ece3cbfd019b265682fb167b8c8fc
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2023-01-31 04:41:43 +0530

    added slide button

--- lib/views/client/onBoard/menu_page.dart
+++ lib/views/client/onBoard/menu_page.dart
@@ -3,6 +3,7 @@ import 'package:bring_the_menu/views/widgets/menu_card.dart';
 import 'package:bring_the_menu/views/widgets/stars.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:slidable_button/slidable_button.dart';
 
 class MyMenu extends StatefulWidget {
   const MyMenu({super.key, required this.documentId});
@@ -40,95 +41,126 @@ class _MyMenuState extends State<MyMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("Quantities: $_quantities");
-        },
-      ),
-      backgroundColor: constants.backgroundColor,
-      appBar: AppBar(
-        elevation: 0.0,
-        actions: [
-          TextButton(
+        backgroundColor: constants.backgroundColor,
+        appBar: AppBar(
+          elevation: 0.0,
+          actions: [
+            TextButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.notifications_sharp,
+                  color: constants.whiteTextColor,
+                )),
+          ],
+          centerTitle: true,
+          title: Text(
+            "hi $docID",
+            // "Bring The Menu",
+            style: const TextStyle(fontSize: 13),
+          ),
+          backgroundColor: Colors.transparent,
+          leading: TextButton(
               onPressed: () {},
               child: Icon(
-                Icons.notifications_sharp,
+                Icons.menu,
                 color: constants.whiteTextColor,
               )),
-        ],
-        centerTitle: true,
-        title: Text(
-          "hi $docID",
-          // "Bring The Menu",
-          style: const TextStyle(fontSize: 13),
-        ),
-        backgroundColor: Colors.transparent,
-        leading: TextButton(
-            onPressed: () {},
-            child: Icon(
-              Icons.menu,
-              color: constants.whiteTextColor,
-            )),
-        bottom: PreferredSize(
-          preferredSize: const Size(50, 40),
-          child: Column(
-            children: [
-              Text(
-                "The Restaurant",
-                style: TextStyle(
-                    fontSize: 22,
-                    color: constants.whiteTextColor,
-                    fontWeight: FontWeight.bold),
-              ),
-              Divider(
-                height: 5,
-                color: constants.themeColor,
-              ),
-            ],
+          bottom: PreferredSize(
+            preferredSize: const Size(50, 40),
+            child: Column(
+              children: [
+                Text(
+                  "The Restaurant",
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: constants.whiteTextColor,
+                      fontWeight: FontWeight.bold),
+                ),
+                Divider(
+                  height: 5,
+                  color: constants.themeColor,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(100, (index) {
-          _quantities.add(0);
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: MenuCard(
-                  index: index,
-                  onQuantityChanged: addQuantity,
-                  rating: 4,
-                  dishName: 'Chicken Biriyani',
-                  isVeg: false,
-                  price: 250,
-                  initQty: 0),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(100, (index) {
+            _quantities.add(0);
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: MenuCard(
+                    index: index,
+                    onQuantityChanged: addQuantity,
+                    rating: 4,
+                    dishName: 'Chicken Biriyani',
+                    isVeg: false,
+                    price: 250,
+                    initQty: 0),
+              ),
+            );
+          }),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            color: constants.menuCardColor,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 15.0, bottom: 15.0, left: 15.0, right: 15.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 50.0,
+                      spreadRadius: 1,
+                      offset: Offset(
+                        0.2,
+                        0.2,
+                      ),
+                    )
+                  ],
+                ),
+                child: HorizontalSlidableButton(
+                  width: MediaQuery.of(context).size.width - 30,
+                  height: 60.0,
+                  buttonWidth: MediaQuery.of(context).size.width / 2,
+                  color: constants.menuCardColor,
+                  buttonColor: constants.themeColor,
+                  dismissible: false,
+                  label: Center(
+                      child: Text(
+                    'Order',
+                    style: TextStyle(
+                        color: constants.whiteTextColor, fontSize: 22),
+                  )),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 30.0),
+                      child: Text(
+                        'Swipe >>',
+                        style: TextStyle(
+                            color: constants.whiteTextColor, fontSize: 22),
+                      ),
+                    ),
+                  ),
+                  onChanged: (position) {
+                    setState(
+                      () {
+                        if (position == SlidableButtonPosition.end) {
+                          print("sliding");
+                          print("Quantities: $_quantities");
+                        }
+                      },
+                    );
+                  },
+                ),
+              ),
             ),
-          );
-        }),
-      ),
-    );
+          ),
+        ));
   }
-}
-
-class TrianglePainter extends CustomPainter {
-  TrianglePainter({required this.veg});
-  final Color veg;
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = veg
-      ..style = PaintingStyle.fill;
-
-    var path = Path();
-    path.moveTo(size.width, 0);
-    path.lineTo(size.width, size.height / 4.5);
-    path.lineTo(size.width / 1.5, 0);
-    path.close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
